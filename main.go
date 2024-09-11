55c79496c8866c84ecaaf54f690e0194fb5ddd7c
sihyunjojo <si4018@naver.com>
2024-09-11 14:30:01 +0900

    refactor: 현재 코드에 맞게 메인 코드 수정

--- main.go
+++ main.go
@@ -1,49 +1,94 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"log/logwriter"
-	"net/http"
+	"log-collector/config"
 	"os"
 
-	// godotenv는 .env 파일에 정의된 키-값 쌍을 환경 변수로 설정하는 라이브러리입니다.
-	"github.com/joho/godotenv" // godotenv 패키지
-	"log/errors"               // 패키지 경로 수정
-	"log/handlers"             // 패키지 경로 수정
-)
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 
-var port string
+	"log-collector/router" // 라우터 임포트
+)
 
 func main() {
 	// .env 파일 로드
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatalf(".env 파일을 로드하는데 실패했습니다: %v", err)
 	}
+	// 로그 설정
+	logger := config.SetupLogger("logs", "logs")
+	config.RotateLogger(logger, "logs") // 로그 파일 롤링 작업 (날짜 기반)
 
-	// 포트 번호 환경 변수로 관리
-	port, err = getPort()
-	if err != nil {
-		log.Fatal(err) // 에러가 발생하면 프로그램을 중단하고 로그 출력
-	}
+	// 로그 출력 설정
+	log.SetOutput(logger) // 모든 로그를 logger로 설정
 
-	// 자정마다 로그 파일을 변경하는 작업을 백그라운드에서 실행
-	go logwriter.ScheduleLogRotation()
+	// Fiber 인스턴스 생성
+	app := fiber.New()
 
-	// 경로 설정: /log/watch 경로에 대한 핸들러 등록
-	http.HandleFunc("/log/watch", handlers.HandleWatchLog) // 패키지 이름을 명시적으로 사용
+	// 라우터 설정
+	router.SetupRoutes(app)
 
-	// 서버 시작
-	fmt.Printf("Starting server on :%s...\n", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	// 서버 실행
+	port := os.Getenv("SERVER_PORT")
+	log.Fatal(app.Listen(":" + port))
 }
 
-// getPort 함수: 환경 변수가 없으면 커스텀 에러 발생
-func getPort() (string, error) {
-	port := os.Getenv("LOG_SERVER_PORT")
-	if port == "" {
-		return "", customerrors.NewPortNotFoundError("LOG_SERVER_PORT")
-	}
-	return port, nil
-}
+//package main
+//
+//import (
+//	"fmt"
+//	"log"
+//	"log-collector/config"
+//	"net/http"
+//
+//	"github.com/gofiber/fiber/v2"
+//
+//	customerrors "log-collector/errors"
+//	"log-collector/writer"
+//	"os"
+//
+//	// godotenv는 .env 파일에 정의된 키-값 쌍을 환경 변수로 설정하는 라이브러리입니다.
+//	"github.com/joho/godotenv" // godotenv 패키지
+//)
+//
+//var port string
+//
+//func main() {
+//	// .env 파일 로드
+//	err := godotenv.Load()
+//	if err != nil {
+//		log.Fatal("Error loading .env file")
+//	}
+//
+//	// Fiber 인스턴스 생성
+//	app := fiber.New()
+//
+//	userLogger := config.SetupLogger()
+//
+//	// 포트 번호 환경 변수로 관리
+//	port, err = getPort()
+//	if err != nil {
+//		log.Fatal(err) // 에러가 발생하면 프로그램을 중단하고 로그 출력
+//	}
+//
+//	// 자정마다 로그 파일을 변경하는 작업을 백그라운드에서 실행
+//	go writer.ScheduleLogRotation()
+//
+//	// 경로 설정: /log/watch 경로에 대한 핸들러 등록
+//	http.HandleFunc("/log/watch", handlers.HandleWatchLog) // 패키지 이름을 명시적으로 사용
+//
+//	// 서버 시작
+//	fmt.Printf("Starting server on :%s...\n", port)
+//	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+//}
+//
+//// getPort 함수: 환경 변수가 없으면 커스텀 에러 발생
+//func getPort() (string, error) {
+//	port := os.Getenv("LOG_SERVER_PORT")
+//	if port == "" {
+//		return "", customerrors.NewPortNotFoundError("LOG_SERVER_PORT")
+//	}
+//	return port, nil
+//}
