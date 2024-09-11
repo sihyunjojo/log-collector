1ce1b78083fb8382ebd8360b930e03b74b461c4a
조시현 <si4018@naver.com>
2024-09-11 17:26:11 +0900

    feat: 사용자별 로그 추가

--- handler/log.go
+++ handler/log.go
@@ -7,23 +7,24 @@ import (
 	"log-collector/config" // 로그 파일에 쓰기 위한 패키지
 )
 
-type WatchLogRequest struct {
+type KeywordLogRequest struct {
 	Keyword string `json:"keyword"`
 }
 
-// HandleWatchLog는 요청의 Body에서 keyword를 추출하고 로그로 저장하는 핸들러
-// @Summary Log a keyword from request body
-// @Description Extracts a keyword from the request body and logs it to a file
-// @Tags logs
+// @Param <name> <location> <type> <required> <description>
+
+// @Summary 키워드 로그 수집
+// @Description 키워드를 받아와서 파일에 기록합니다.
+// @Tags 로그
 // @Accept  json
 // @Produce  json
-// @Param   keyword  body WatchLogRequest true "Keyword to log"
+// @Param  keyword body string true "로그로 사용할 키워드"
 // @Success 200 {object} map[string]interface{}
 // @Failure 400 {object} map[string]string
 // @Router /log/keyword [post]
-func HandleWatchLog(c *fiber.Ctx) error {
+func HandleKeywordLog(c *fiber.Ctx) error {
 	// 요청 Body에서 JSON 데이터 추출
-	var req WatchLogRequest
+	var req KeywordLogRequest
 	if err := c.BodyParser(&req); err != nil {
 		// 오류가 발생하면 400 Bad Request 응답
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
@@ -39,5 +40,40 @@ func HandleWatchLog(c *fiber.Ctx) error {
 	// 성공적으로 처리되었음을 응답
 	return c.JSON(fiber.Map{
 		"keyword": req.Keyword,
+	})
+}
+
+// @Summary 사용자별 키워드 로그 수집
+// @Description 사용자별 키워드를 받아와서 파일에 기록합니다.
+// @Tags 로그
+// @Accept  json
+// @Produce  json
+// @Param  memberId  path  string  true  "사용자 아이디(사용자 유니크 값)"
+// @Param  keyword body string true "로그로 사용할 키워드"
+// @Success 200 {object} map[string]interface{}
+// @Failure 400 {object} map[string]string
+// @Router /log/keyword/{memberId} [post]
+func HandleKeywordLogByMember(c *fiber.Ctx) error {
+	// 경로에서 userId 추출
+	memberId := c.Params("memberId")
+
+	// 요청 Body에서 JSON 데이터 추출
+	var req KeywordLogRequest
+	if err := c.BodyParser(&req); err != nil {
+		// 오류가 발생하면 400 Bad Request 응답
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid request body",
+		})
+	}
+
+	// 로그 파일에 keyword 저장
+	userLogger := config.SetupLogger("keywordByMember", "keywordByMember")
+	log.SetOutput(userLogger)
+	log.Printf("MembmerId: %s, Received keyword: %s", memberId, req.Keyword)
+
+	// 성공적으로 처리되었음을 응답
+	return c.JSON(fiber.Map{
+		"memberId": memberId,
+		"keyword":  req.Keyword,
 	})
 }
