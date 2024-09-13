14e91861690225fa6b7cb2a0f4f93232712b5a81
조시현 <si4018@naver.com>
2024-09-13 14:36:31 +0900

    fix: timefunc 에러 해결

--- config/setup.go
+++ config/setup.go
@@ -57,8 +57,5 @@ func GetLogger() logger.Config {
 		Output:     userLogger,
 		TimeFormat: "2006-01-02 15:04:05",
 		TimeZone:   seoulLocation.String(),
-		TimeFunc: func() time.Time {
-			return time.Now().In(SeoulLocation)
-		},
 	}
 }
